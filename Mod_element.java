a902280d4bcbb6b7e7d5a5ee01b5e5d8e8233641
INDUSAKTHIVEL <indusakthi2003@gmail.com>
2023-07-19 11:29:38 +0530

    modelement

---
+++ Mod_element.java
@@ -0,0 +1,29 @@
+package Mod_array;
+
+import java.util.Scanner;
+
+public class Mod_element {
+	
+
+	public static void main(String[] args) {
+			    Scanner sc = new Scanner(System.in);
+			    StringBuilder s = new StringBuilder();
+			    System.out.println("Enter the length:");
+			    int n = sc.nextInt();
+			    System.out.println("Enter B:");
+		        int b = sc.nextInt();	    
+			    int[] a=new int[n];
+			    System.out.println("Enter the array elements:");
+			    for(int i = 0;i<n;i++){
+			        a[i] = sc.nextInt();
+			    }
+			    for(int j=0; j<n; j++){
+			        s.append(a[j]);
+			    }
+			    String num = s.toString();
+			    int num1 = Integer.parseInt(num);
+			    System.out.println(num1%b);
+
+			    }
+
+ }
